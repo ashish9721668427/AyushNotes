3b1f0b6e31950365b95428f62e6860b2fb9c1f6e
Ashishdev97 <ashishumaraodev97@gmail.com>
2026-04-17 00:29:41 +0530

    Initial commit

---
+++ src/main/java/com/codewithashishsample/ayushnotes/AyushNotesApplication.java
@@ -0,0 +1,13 @@
+package com.codewithashishsample.ayushnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyushNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AyushNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithashishsample/ayushnotes/AyushNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithashishsample.ayushnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyushNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
