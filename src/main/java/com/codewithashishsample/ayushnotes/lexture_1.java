383497140f90628c10b5e14b98d4464b7fdcc4d8
Ashishdev97 <ns208011@gmail.com>
2026-04-18 18:13:41 +0530

    lexture-1

---
+++ src/main/java/com/codewithashishsample/ayushnotes/lexture_1.java
@@ -0,0 +1,14 @@
+package com.codewithashishsample.ayushnotes;
+
+public class lexture_1 {
+    public static void main(String[] args) {
+
+
+    int a= 10;
+    double d=10.7;
+    char c='a';
+    System.out.println(a);
+    System.out.println(d);
+    System.out.println(c);
+    }
+}
